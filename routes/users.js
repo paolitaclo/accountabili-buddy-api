73269d946a0fd5ac9cdb4307e9f19dc3b1a2319f
Kevin Lam <kevinlam.ucsb@gmail.com>
2017-04-22 14:01:11 -0700

    setting up boilerplate

---
+++ routes/users.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const knex = require('../knex');
+
+router.get('/users', (req, res, next) => {
+  // write code in here
+  let magic = 'happen';
+})
+
+router.post('/users', (req, res, next) => {
+  // write code in here
+  let magic = 'happen';
+})
+
+module.exports = router;
