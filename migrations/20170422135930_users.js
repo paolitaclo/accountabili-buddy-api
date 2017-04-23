6bdce023c535cb4ca1159373a3134e4aeb660c5d
Paola Claros <paolita.claros@gmail.com>
2017-04-23 16:33:19 -0700

    adding schema for user table

--- migrations/20170422135930_users.js
+++ migrations/20170422135930_users.js
@@ -1,10 +1,11 @@
 
-exports.up = function(knex, Promise) {
-  return knex.schema.createTable('users', (table) => {
-    // fill in the magic
-  })
-};
+exports.up = knex.schema.createTable('users', (table) => {
+  table.increments('id').primary();
+  table.string('user_name').notNullable().defaultTo('');
+  table.string('first_name').notNullable().defaultTo('');
+  table.string('last_name').notNullable().defaultTo('');
+  table.string('email').notNullable().unique();
+  table.specificType('hashed_password', 'char(60)').notNullable();
+});
 
-exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('users');
-};
+exports.down = knex => knex.schema.dropTable('users');
