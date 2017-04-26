fc15f24ade8a882a9835bfbac2cac1544fdacf8f
Paola <paolita.claros@gmail.com>
2017-04-26 14:03:58 -0700

    updating from upstream master

--- models/teams.js
+++ models/teams.js
@@ -5,19 +5,9 @@ require('./users_teams');
 
 const Teams = Bookshelf.Model.extend({
   tableName: 'teams',
-<<<<<<< HEAD
   users: function() {
     return this.belongsToMany('Users').through('UsersTeams');
   }
 });
-||||||| merged common ancestors
-  users: () => this.belongsToMany('Users').through('UsersTeams')
-});
-=======
-  users: function() {
-    return this.belongsToMany('Users').through('UsersTeams');
-}
-})
->>>>>>> 3233d52016d3915513161df8e93141ee1cc6b02c
 
 module.exports = Bookshelf.model('Teams', Teams);
