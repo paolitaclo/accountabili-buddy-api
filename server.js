5505bfd1a9fabb1a2150afb1492d4838676c9f34
Paola <paolita.claros@gmail.com>
2017-04-27 16:45:34 -0700

    draft for FB Oauth

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const port = process.env.PORT || 9001;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook').Strategy;
 
 switch (app.get('env')) {
   case 'development':
@@ -22,8 +24,43 @@ switch (app.get('env')) {
   default:
 }
 
+passport.use(new FacebookStrategy({
+  clientID: process.env.FACEBOOK_APP_ID,
+  clientSecret: process.env.FACEBOOK_APP_SECRET,
+  callbackURL: 'http://localhost:9001/users/facebook/return'
+}, function (accessToken, refreshToken, profile, cb) {
+  return cb(null, profile);
+}));
+
+passport.serializeUser(function (user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function (obj, cb) {
+  cb(null, obj);
+});
+
 app.use(bodyParser.json());
-app.use(cookieParser());
+app.use(cookieParser()); // come back later here
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/users/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/users/facebook/return',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+  app.get('/profile',
+    require('connect-ensure-login').ensureLoggedIn(),
+    function(req, res){
+      res.send({ user: req.user });
+    });
 
 const users = require('./routes/users');
 const teams = require('./routes/teams');
