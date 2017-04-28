6f07fc554e60fb9bb1a5acd1b7a880c317903663
Paola <paolita.claros@gmail.com>
2017-04-27 20:18:20 -0700

    changing htttp to https in teams seeds

--- seeds/2_teams.js
+++ seeds/2_teams.js
@@ -8,21 +8,21 @@ exports.seed = function(knex, Promise) {
         {
           id: 1,
           name: 'g42',
-          team_image_url: 'http://www.galvanize.com/wp-content/themes/galvanize/img/galvanize-g.svg',
+          team_image_url: 'https://media.glassdoor.com/sqll/825775/galvanize-squarelogo-1429039425588.png',
           created_at: new Date('2016-06-26 14:26:16 UTC'),
           updated_at: new Date('2016-06-26 14:26:16 UTC')
         },
         {
           id: 2,
           name: 'g52',
-          team_image_url: 'http://www.galvanize.com/wp-content/themes/galvanize/img/galvanize-g.svg',
+          team_image_url: 'https://media.glassdoor.com/sqll/825775/galvanize-squarelogo-1429039425588.png',
           created_at: new Date('2016-06-26 14:26:16 UTC'),
           updated_at: new Date('2016-06-26 14:26:16 UTC')
         },
         {
           id: 3,
           name: 'g Web Instructors',
-          team_image_url: 'http://www.galvanize.com/wp-content/themes/galvanize/img/galvanize-g.svg',
+          team_image_url: 'https://media.glassdoor.com/sqll/825775/galvanize-squarelogo-1429039425588.png',
           created_at: new Date('2016-06-26 14:26:16 UTC'),
           updated_at: new Date('2016-06-26 14:26:16 UTC')
         }
